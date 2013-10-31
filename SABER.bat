b4f14dab7c9325291b94a01ee6a165e814c11b3b
Daemon L. Schmidt <spysoldier@gmail.com>
2013-10-31 12:46:56 -0700

    Create SABER.bat

---
+++ SABER.bat
@@ -0,0 +1,22 @@
+@echo off
+echo I am a citizen of the interwebs. 
+IF "%~1"=="/s" cleanmgr /sageset:0
+cleanmgr /sagerun:0
+echo Code is my body, and design is my blood. 
+IF "%~1"=="/d" defrag C: /b /h /u /v
+echo I have posted over nine thousand memes. 
+defrag /c /x /w /h /u /v
+echo Unknown to Herp, nor to Derp. 
+echo y|chkdsk /r
+chkdsk D: /x /r
+chkdsk E: /x /r
+chkdsk F: /x /r
+echo Have withstood corrupted images to create many lols. 
+chkdsk G: /x /r
+chkdsk H: /x /r
+chkdsk i: /x /r
+chkdsk J: /x /r
+echo Yet, people contintue to be confuzzled by me. 
+shutdown -r -t 60
+echo So as I pray, Unlimited Daemon Works! 
+echo LOLWUT? 
